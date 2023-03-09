253526085d05caff3fa82bfec57c598365f17f84
Yang <74367412+G2Yang@users.noreply.github.com>
2023-03-09 13:30:14 +0100

    Add files via upload

---
+++ PracticaWpf/PracticaWpf/ApartadoC.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PracticaWpf
+{
+    /// <summary>
+    /// Lógica de interacción para ApartadoC.xaml
+    /// </summary>
+    public partial class ApartadoC : Window
+    {
+        public ApartadoC()
+        {
+            InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Nom = "Judit",Cognom = "San Garcia" ,Adreça = "Avinguda meridiana, 153", Població = "08001", DNI = "12345678Z", NHC = 1});
+            lvUsers.ItemsSource = items;
+        }
+    }
+}
---
+++ PracticaWpf/PracticaWpf/User.cs
@@ -0,0 +1,16 @@
+﻿namespace PracticaWpf
+{
+    internal class User
+    {
+        public User()
+        {
+        }
+
+        public string Nom { get; set; }
+        public string Cognom { get; set; }
+        public string Adreça { get; set; }
+        public string Població { get; set; }
+        public string DNI { get; set; }
+        public int NHC { get; set; }
+    }
+}
